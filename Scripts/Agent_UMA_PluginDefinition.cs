d37a83eeb944a64c53950debe1b6b2e3e6bc9deb
Ross Gardler <rgardler@apache.org>
2019-06-09 01:12:44 -0700

    WIP: Initial skeleton of a plugin.

---
+++ Scripts/Agent_UMA_PluginDefinition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using wizardscode.plugin;
+
+namespace wizardscode.plugin {
+    public class Agent_UMA_PluginDefinition : AbstractPluginDefinition
+    {
+        public override PluginCategory GetCategory()
+        {
+            return PluginCategory.Agent;
+        }
+
+        public override Type GetManagerType()
+        {
+            return typeof(Agent_PluginManager);
+        }
+
+        public override string GetPluginImplementationClassName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetProfileTypeName()
+        {
+            return "UMA_Agent_Profile";
+        }
+
+        public override string GetReadableName()
+        {
+            return "UMA Agent";
+        }
+
+        public override string GetURL()
+        {
+            return "https://assetstore.unity.com/packages/3d/characters/uma-2-unity-multipurpose-avatar-35611";
+        }
+    }
+}
---
+++ Scripts/UMA_Agent_Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace wizardscode.plugin
+{
+    [CreateAssetMenu(fileName = "UMA_Agent_Profile", menuName = "Wizards Code/Agent/UMA")]
+    class UMA_Agent_Profile : AbstractPluginProfile
+    {
+
+    }
+}

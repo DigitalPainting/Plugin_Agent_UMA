c9a0c6d603d604330455f695340f5f964b2ce3fb
Ross Gardler <rgardler@apache.org>
2019-06-09 10:32:21 -0700

    Generate a random avatar at startup

--- Scripts/UMASettingSO.cs
+++ Scripts/UMASettingSO.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
+using UMA;
+using UMA.CharacterSystem;
+using static UMA.CharacterSystem.DynamicCharacterAvatar;
 
 namespace wizardscode.validation
 {

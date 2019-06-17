f672884a461fbd8e7701f9b6a407a03b69d6c15b
Ross Gardler <rgardler@apache.org>
2019-06-16 20:46:31 -0700

    setup the RandomAvatar correctly if it is used

--- Scripts/UMASettingSO.cs
+++ Scripts/UMASettingSO.cs
@@ -8,5 +8,17 @@ namespace wizardscode.validation
     [CreateAssetMenu(fileName = "UMASettingSO_DESCRIPTIVENAME", menuName = "Wizards Code/Validation/UMA/UMA Setting SO")]
     public class UMASettingSO : AgentSettingSO
     {
+
+        internal override void InstantiatePrefab()
+        {
+            base.InstantiatePrefab();
+            UMARandomAvatar randomAvatar = Instance.GetComponent<UMARandomAvatar>();
+            if (randomAvatar)
+            {
+                randomAvatar.NameBase = agentName;
+                randomAvatar.ParentObject = Instance;
+                PositionOnGround(spawnPosition);
+            }
+        }
     }
 }
